422aa27868795ac59de742f4f2106a784de1d404
yoonji-k <66683691+yoonji-k@users.noreply.github.com>
2022-05-30 00:52:47 +0900

    Add files via upload

---
+++ seperate.py
@@ -0,0 +1,19 @@
+import glob
+import os
+import pandas as pd
+
+
+for filename in glob.glob("valid__malware/*.csv"):
+    with open(os.path.join(os.getcwd(), filename), 'r') as f:
+        file = ''
+        while True:
+            line = f.readline()
+            split_num = line.split(" ")
+       # print(split_num[0])
+            file += split_num[0] + '\n'
+            if line == '': 
+                f = open(os.path.join(os.getcwd(), filename), 'w')
+                f.write(file)
+                f.close()
+                break
+   
